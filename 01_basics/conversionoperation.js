ca7ea9c3e274fde94ff020a75c4a21e3047856db
Vikas Yadav <123307610+vikasyadav01234@users.noreply.github.com>
2024-02-08 17:36:25 +0000

    main

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,8 @@
+let score = "33";
+console.log(typeof score);
+
+let valueNumber = Number(score)
+console.log(typeof valueNumber);
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
