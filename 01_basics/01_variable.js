462dac4a50d589e0d7c5b87c6c31b3d9126b39d9
Vikas Yadav <123307610+vikasyadav01234@users.noreply.github.com>
2024-02-08 16:52:11 +0000

    data types

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,7 @@
+const accountId = 14444
+let accountEmail="heehud@gmil.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+//acountId = 2
+console.table([accountId, accountEmail, accountPassword, accountCity])
