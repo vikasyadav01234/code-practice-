895441e3ef9828332436d32d08a3776a64b5a7ed
Vikas Yadav <123307610+vikasyadav01234@users.noreply.github.com>
2024-02-09 06:40:39 +0000

    main

--- 01_basics/conversionoperation.js
+++ 01_basics/conversionoperation.js
@@ -6,3 +6,22 @@ console.log(typeof valueNumber);
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn)
+//*******************************************operations *********************************
+console.log(2+2)
+console.log(2-2)
+console.log(2/2)
+console.log(2*2)
+console.log(2**2)
+console.log(2%2)
+
+let str1="Vikas"
+let str2=" Yadav"
+let str3=str1+str2
+console.log(str3)
+
+
+
+(//comparision
+/*console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); */
