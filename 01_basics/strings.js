17d667a35be8f37973c063ef26c958a20d5fded2
Vikas Yadav <123307610+vikasyadav01234@users.noreply.github.com>
2024-02-10 16:55:56 +0000

    main

---
+++ 01_basics/strings.js
@@ -0,0 +1,8 @@
+const name = "Vikas Yadav";
+const repocount = 50;
+console.log(name+repocount+"value");
+
+console.log(`Hello My name is ${name} and my repo number is ${repocount}`);
+const gameName = new String('Vikas Yadav')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
