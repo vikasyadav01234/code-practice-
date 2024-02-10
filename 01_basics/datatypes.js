502e469c914f1e5d8ca2d583fb20965d7a1916ca
Vikas Yadav <123307610+vikasyadav01234@users.noreply.github.com>
2024-02-10 15:54:09 +0000

    main

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -15,3 +15,24 @@ let isLoggedIn = false
 //object
 
 console.log(typeof age)
+
+//stack (premitive), heap (non-premitive)
+let myYoutubename = "Vikasyadav.com";
+
+let another = myYoutubename;
+console.log(another);
+another = "chai or code";
+console.log(myYoutubename)
+console.log(another)
+
+let userOne = {
+    email: "yadav2911@gmail.com",
+    upi: "7073964925@fam"
+}
+
+let userTwo = userOne;
+console.log(userOne);
+console.log(userTwo);
+userTwo.email = "vikas@gmail.com";
+console.log(userOne);
+console.log(userTwo);
