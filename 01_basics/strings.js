e9e5480b4b94b39d155cc11f993615682c7be061
Vikas Yadav <123307610+vikasyadav01234@users.noreply.github.com>
2024-02-11 06:22:03 +0000

    main

--- 01_basics/strings.js
+++ 01_basics/strings.js
@@ -6,3 +6,8 @@ console.log(`Hello My name is ${name} and my repo number is ${repocount}`);
 const gameName = new String('Vikas Yadav')
 console.log(gameName[0]);
 console.log(gameName.__proto__);
+
+console.log(gameName.toLocaleUpperCase());
+console.log(gameName.charAt(5));
+
+console.log(gameName.indexOf('Ya'));
