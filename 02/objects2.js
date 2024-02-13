0c432ff8f6c0871bc3f69e15b5dd4804ec02d1d5
Vikas Yadav <123307610+vikasyadav01234@users.noreply.github.com>
2024-02-13 06:30:08 +0000

    main

---
+++ 02/objects2.js
@@ -0,0 +1,26 @@
+const tinderUser = new Object()
+tinderUser.id = "124334fd"
+tinderUser.email = "fcdmdmi@gmail.com"
+
+
+console.log(tinderUser);
+
+const reqularUser = {
+    email: "vikas@gamil.com",
+    fullname: {
+        username: {
+            name: "vikas",
+            lastname: "yadav",
+        },
+        tel: 700777707
+    }
+}
+console.log(reqularUser);
+console.log(reqularUser.fullname.username.lastname)
+
+// combine of obj1 and obj2
+ const obj1 = { 1:"a",2:"b",3:"c",4:"d"}
+ const obj2 = {5:"f",6:"t"};
+
+ const obj3 = Object.assign({},obj1, obj2)
+ console.log(obj3);
