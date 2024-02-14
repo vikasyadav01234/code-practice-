e1de25eedcc54c8d8787bfa86d4e9f562283d0dd
Vikas Yadav <123307610+vikasyadav01234@users.noreply.github.com>
2024-02-14 06:08:10 +0000

    main

---
+++ 02/function.js
@@ -0,0 +1,13 @@
+function sayMyName(){
+    console.log("V");
+    console.log("I");
+    console.log("K");
+    console.log("A");
+    console.log("S");
+}
+sayMyName()
+
+function addnumber(n1 ,n2){
+    console.log(n1+n2);
+}
+addnumber(5,0)
