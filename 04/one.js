514b3ea2dcf006b748b186c52c6727e7426a0816
Vikas Yadav <123307610+vikasyadav01234@users.noreply.github.com>
2024-02-15 06:47:25 +0000

    main

---
+++ 04/one.js
@@ -0,0 +1,8 @@
+const balance = 1000;
+if (balance<500){
+    console.log("Balance is less then 500");
+}else if(balance<750){
+    console.log("Balance is less then 750");
+}else{
+    console.log("Balance is less then 2000");
+}
